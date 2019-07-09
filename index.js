01aa4fac033b8f792920c0dd9570a783feab4e56
othaderek <otha.hernandez@gmail.com>
2019-07-09 13:53:42 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,49 @@
+fetch('https://opentdb.com/api.php?amount=10&category=23')
+.then(res => res.json())
+.then(triviaJson)
+
+const triviaUl = document.querySelector('.trivia')
+const choicesForm = document.querySelector('.choices')
+
+
+function triviaJson(trivia) {
+    // debugger
+    const triviaArr = trivia.results
+    for (trivia of triviaArr) {
+        triviaUl.innerHTML +=
+            `
+            <h4>Question</h4>
+            <p>${trivia.question}</p>
+            <div class="radio-div">${displayOptions(trivia)}</div>
+
+            `
+        }
+
+        for (trivia of triviaArr) {
+
+            choicesForm.innerHTML +=
+            `
+                <input type="radio" name="gender" value="male"> ${displayOptions(trivia)}<br>
+
+            `
+    }
+
+}
+
+function displayOptions(ansArray){
+    const choices = ansArray.incorrect_answers
+    const correctAnswer = ansArray.correct_answer
+    choices.push(correctAnswer)
+
+    let counter = 1;
+    // debugger
+    return choices.map(ans => {
+        return `
+        <input type="radio" id="option${++counter}" name="choice" value="${ans}">
+        <label for="option${++counter}">${ans}</label><br>
+        `
+    }).join(" ")
+}
+
+const checked = document.querySelector('input[name="choice"]:checked').value;
+console.log(checked)
